6fb1d3457561bbd6239ccce81fb38a7c5a694921
fe0b6 <spirit-dnk@yandex.ru>
2018-10-04 07:43:09 +0300

    up

---
+++ borg.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+NAME=`/bin/date '+%Y-%m-%d'`
+REPONAME=$1
+BACKUPPATHS=$2
+
+/usr/bin/borg create $REPONAME::$NAME $BACKUPPATHS
+/usr/bin/borg prune --keep-daily=7 --keep-weekly=4 --keep-monthly=12 --keep-yearly=10 $REPONAME
